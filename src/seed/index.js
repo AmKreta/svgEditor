ce54b795ab1f1c46ed37e11a0bbfb68a370e1064
AmKreta <kamresh485@gmail.com>
2022-09-04 16:51:49 +0530

    added sample design

--- src/seed/index.js
+++ src/seed/index.js
@@ -2,12 +2,14 @@ import skills from './Skills-0.json';
 import introduction from './Introduction-0.json';
 import experience from './Experience-0.json';
 import projectInspiration from './Project Inspiration-0.json';
+import foodCartSample from './Food-cart-0.json';
 
 const projects = {
     introduction,
     skills,
     experience,
-    projectInspiration
+    projectInspiration,
+    foodCartSample
 }
 
 export const personalProjectIds = Object.keys(projects).map(name => projects[name].id);
